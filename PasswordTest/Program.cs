fc37446eeb7d49efbedb0358fbc9fcbbd7a43fe5
Joween Flores <4754752+joweenflores@users.noreply.github.com>
2020-02-11 16:10:21 +0800

    Added Demo Console App

---
+++ PasswordTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PasswordTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Create Secure Password Hash
+            Console.WriteLine("Enter Password: ");
+            var textPassword = Console.ReadLine();
+            var hashedPassword = PasswordUtility.PasswordHelper.Hash(textPassword);
+            Console.WriteLine("Hashed Password: {0}", hashedPassword);
+           
+            Console.WriteLine("\n====================================");
+            Console.WriteLine("Now, you will need to type the password again to verify. Press any key continue...");
+            Console.WriteLine("====================================\n");
+            Console.ReadKey();
+            
+            //Verify
+            Console.WriteLine("Enter Password Again to Verify: ");
+            textPassword = Console.ReadLine();
+            Console.WriteLine(PasswordUtility.PasswordHelper.Verify(textPassword, hashedPassword) ? "SUCCESS: Password Match!" : "ERROR: Password Mismatch!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PasswordUtility/PasswordHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace PasswordUtility
+{
+    public static class PasswordHelper
+    {
+        public static int SaltSize { get; set; } = 256;
+        public static int HashSize { get; set; } = 32;
+        public static int Iterations { get; set; } = 1000;
+
+        private static string Hash(string password, int iterations)
+        {
+            // Create Salt
+            var salt = new byte[SaltSize];
+            new RNGCryptoServiceProvider().GetBytes(salt);
+
+            // Create Hash
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            var hash = pbkdf2.GetBytes(HashSize);
+
+            // Combine Salt and Hash
+            var hashBytes = new byte[SaltSize + HashSize];
+            Array.Copy(salt, 0, hashBytes, 0, SaltSize);
+            Array.Copy(hash, 0, hashBytes, SaltSize, HashSize);
+
+            // Convert to Base64
+            var base64Hash = Convert.ToBase64String(hashBytes);
+            return base64Hash;
+        }
+
+        public static string Hash(string password)
+        {
+            return Hash(password, Iterations);
+        }
+
+        public static bool Verify(string password, string hashedPassword)
+        {
+            // Get Hash Bytes
+            var hashBytes = Convert.FromBase64String(hashedPassword);
+
+            // Get Saved Salt
+            var salt = new byte[SaltSize];
+            Array.Copy(hashBytes, 0, salt, 0, SaltSize);
+
+            // Create Hash with Saved Salt 
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, Iterations);
+            var hash = pbkdf2.GetBytes(HashSize);
+
+            // Get Result
+            for (var i = 0; i < HashSize; i++)
+                if (hashBytes[i + SaltSize] != hash[i])
+                    return false;
+            return true;
+        }
+    }
+}
